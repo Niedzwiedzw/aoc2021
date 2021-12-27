cd238bba93fcee865df62dc9002c254f61bab5f1
niedzwiedzw <niedzwiedzwo@gmail.com>
2021-12-27 13:01:40 +0100

    formatting

--- examples/day1.rs
+++ examples/day1.rs
@@ -3,21 +3,30 @@ use itertools::Itertools;
 const DATA: &str = include_str!("./day1.txt");
 
 fn main() {
-    let lines: Vec<i32> = DATA
-        .split("\n")
-        .filter_map(|n| n.parse().ok())
-        .collect();
+    let lines: Vec<i32> = DATA.split("\n").filter_map(|n| n.parse().ok()).collect();
     // star 1
-    let count = |lines: &Vec<i32>| lines
-        .iter()
-        .zip(lines.iter().skip(1))
-        // .inspect(|(prev, next)| println!("{} < {} :: {}", prev, next, prev < next))
-        .filter(|(prev, next)| prev < next)
-        .count();
-    println!("{} measurements are larger than the previous one", count(&lines));
+    let count = |lines: &Vec<i32>| {
+        lines
+            .iter()
+            .zip(lines.iter().skip(1))
+            // .inspect(|(prev, next)| println!("{} < {} :: {}", prev, next, prev < next))
+            .filter(|(prev, next)| prev < next)
+            .count()
+    };
+    println!(
+        "{} measurements are larger than the previous one",
+        count(&lines)
+    );
 
     // star 2
-    let windows: Vec<_> = lines.iter().zip(lines.iter().skip(1)).zip(lines.iter().skip(2)).map(|((a, b), c)| a + b + c).collect();
-    println!("{} windowed measurements are larger than the previous one", count(&windows));
-
+    let windows: Vec<_> = lines
+        .iter()
+        .zip(lines.iter().skip(1))
+        .zip(lines.iter().skip(2))
+        .map(|((a, b), c)| a + b + c)
+        .collect();
+    println!(
+        "{} windowed measurements are larger than the previous one",
+        count(&windows)
+    );
 }
